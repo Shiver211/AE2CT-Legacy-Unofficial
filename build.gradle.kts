9acd416f5f7d3b00e4299ef0d7c6fd61d2df56ab
Shiver211 <2863677073@qq.com>
2026-09-07 13:09:47 +0800

    修复构建

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("maven-publish")
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
     id("eclipse")
-    id("com.gtnewhorizons.retrofuturagradle") version "1.3.19"
+    id("com.gtnewhorizons.retrofuturagradle") version "1.4.9"
 }
 
 // Project properties
@@ -154,8 +154,7 @@ repositories {
     }
     maven {
         name = "GTNH Maven"
-        url = uri("http://jenkins.usrv.eu:8081/nexus/content/groups/public/")
-        isAllowInsecureProtocol = true
+        url = uri("https://nexus.gtnewhorizons.com/repository/public/")
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,11 @@ pluginManagement {
         maven {
             // RetroFuturaGradle
             name = "GTNH Maven"
-            url = uri("http://jenkins.usrv.eu:8081/nexus/content/groups/public/")
-            isAllowInsecureProtocol = true
+            url = uri("https://nexus.gtnewhorizons.com/repository/public/")
             mavenContent {
                 includeGroup("com.gtnewhorizons")
                 includeGroup("com.gtnewhorizons.retrofuturagradle")
+                includeGroupByRegex("com\\.gtnewhorizons\\..+")
             }
         }
         gradlePluginPortal()
